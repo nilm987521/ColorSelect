7d9d94f5a46fd452e2ed6911764cee095d0eb484
nilm987521 <nilm987521@gmail.com>
2022-01-14 16:37:12 +0800

    add input_range

--- js/colorlist.js
+++ js/colorlist.js
@@ -1,30 +1,28 @@
 var pinned = false;
 $(document).ready(function () {
+    ToCenter()
     //產生顏色方塊
     var br = `<br>`;
+    let i = 0;
     for (let j = 0; j < 16; j++) {
         $('div.colorlist').append(`<div id=\"car${j}\" class='car'></div>`)
         if (j != 0 & j % 4 == 3) {
             $('div.colorlist').append(br)
         }
-    };
-
-    let i = 0;
+    };  
     do {
         $(`div#car${i % 16}`).append(`<div class='color_node' 
-                                            style='background-color:#${DecimalToColorFormat(i)}' 
-                                            alt='#${DecimalToColorFormat(i)}'><\/div>`)
+                                            style='background-color:#${DecimalToColorFormat(i)}'><\/div>`)
         if (i % 256 == 255) {
             $('div.car').append(br)
         }
         i++
     } while (i < 16 * 16 * 16);
     
-    // NOTION on沒有hover可以註冊，須改用mouseenter mouseleave
+    // ColorTag
     $('div.color_node').on("mouseenter",ColorCodeInit);
-    // $('div.color_node').hover(ColorCodeInit);
-    //-----------------------------------------
 
+    // 釘住ColorTag位置
     $('div.color_node').click(function () {
         pinned = !pinned;
         $('div#copy').fadeToggle(100)
@@ -39,25 +37,46 @@ $(document).ready(function () {
                 height: 110,
             },100)
         }
-        
     })
 
+    // 複製顏色代碼
     $('button#copy').click(function() {
         switch($( "input:checked" )[0].value){
             case 'HEX':
-                navigator.clipboard.writeText($('button#copy')[0].value)
+                navigator.clipboard.writeText(RGB2HEX($('span.rgb')[0].innerHTML))
                 break
             case 'RGB':
                 navigator.clipboard.writeText($('span.rgb')[0].innerHTML)
                 break
-        }
-        
+        }  
     })
 
+    // ColorTag跟隨滑鼠
     $(window).mousemove(function (e) {
         MouseMoveInColorList(e)
     });
+
+    // 更改顏色透明度
+    $('input.input-range').change(function() {
+        let hexcolor =  RGB2HEX($('div.largecolor').css('background-color'))
+        $('div.largecolor').css('background-color',hexcolor + DEC2HEX($(this).val()))
+    }) 
+
+    // 視窗大小改變時畫面致中
+    $(window).resize(ToCenter)
 })
+
+// --------------   自訂Function    -----------------
+// --------------   自訂Function    -----------------
+// --------------   自訂Function    -----------------
+// 數字轉顏色格式
+function DecimalToColorFormat(number) {
+    let colorstr = DEC2HEX(number)
+    colorstr = ("000" + colorstr).substr(-3)
+    let colorarr = colorstr.split("")
+    let colorFormat = `${colorarr[2]}0${colorarr[1]}0${colorarr[0]}0`
+    return colorFormat
+}
 
 // 顯示ColorTag
 function MouseMoveInColorList(e) {
@@ -79,22 +98,6 @@ function MouseMoveInColorList(e) {
     } else {
         $('div.colortag').css('display', 'inline-block')
     }
-
-    $(window).resize(ToCenter)
-
-    ToCenter()
-}
-
-// 數字轉顏色格式
-function DecimalToColorFormat(number) {
-    if (number < 0) {
-        number = 0xFFFFFFFF + number + 1;
-    }
-    let colorstr = number.toString(16).toUpperCase()
-    colorstr = ("000" + colorstr).substr(-3)
-    let colorarr = colorstr.split("")
-    let colorFormat = `${colorarr[2]}0${colorarr[1]}0${colorarr[0]}0`
-    return colorFormat
 }
 
 // 顏色展示區塊移動
@@ -126,4 +129,24 @@ function ToCenter() {
     let body_height =  $(window).height()
     $('div.colorlist').css('margin-top',(body_height-552)/2)
     $('div.colorlist').css('margin-left',(body_width-552)/2)
+}
+
+// 顏色RGB格式 轉換 16進制格式
+function RGB2HEX(str) {
+    // TODO　FIX RGBA regex pattern
+    // let regx = /^rgb\((\d{1,3}), (\d{1,3}), (\d{1,3})\)$/
+    let regx = /^rgb.*\((\d{1,3}), (\d{1,3}), (\d{1,3}).*\)/
+    let color_arr = str.match(regx)
+    let res = ''
+    for (let i = 1;i < color_arr.length ; i ++) {
+        res = res + DEC2HEX(parseInt(color_arr[i]))
+    }
+    return '#' + res
+}
+
+// 10進位變16進位
+function DEC2HEX(number){
+  if (number < 0)
+  {number = 0xFFFFFFFF + number + 1;}
+  return number.toString(16).toUpperCase();
 }
