93db1cb109eb71a378dd0b15c5f36266c331804d
1+ <nilm987521@gmail.com>
2022-01-15 11:45:35 +0800

    version 1

--- js/colorlist.js
+++ js/colorlist.js
@@ -1,41 +1,54 @@
 var pinned = false;
+var HEX_COLOR_REGEX = /^\#([0-9,A-F]{2})([0-9,A-F]{2})([0-9,A-F]{2})/
+var HEXA_COLOR_REGEX = /^\#([0-9,A-F]{2})([0-9,A-F]{2})([0-9,A-F]{2})([0-9,A-F]{2})/
+
 $(document).ready(function () {
+    // div.main置中，產生顏色方塊
     ToCenter()
-    //產生顏色方塊
-    var br = `<br>`;
     let i = 0;
     for (let j = 0; j < 16; j++) {
         $('div.colorlist').append(`<div id=\"car${j}\" class='car'></div>`)
         if (j != 0 & j % 4 == 3) {
-            $('div.colorlist').append(br)
+            $('div.colorlist').append(`<br>`)
         }
     };  
     do {
         $(`div#car${i % 16}`).append(`<div class='color_node' 
-                                            style='background-color:#${DecimalToColorFormat(i)}'><\/div>`)
+                                            style='background-color:#${NodeColorGen(i)}'><\/div>`)
         if (i % 256 == 255) {
-            $('div.car').append(br)
+            $('div.car').append(`<br>`)
         }
         i++
     } while (i < 16 * 16 * 16);
     
-    // ColorTag
-    $('div.color_node').on("mouseenter",ColorCodeInit);
+    // 移動ColorTag，並設定顏色
+    $('div.color_node').on("mouseenter",function() {
+        if (!pinned) {
+            SetColorTagPosition($(this));
+            SetLargeColor(
+                RGB2HEX($('div.color_node:hover').css("background-color")) 
+                + DEC2HEX(parseInt($('input.input-range').val()))
+            );
+            $('span.rgb').html($('div.largecolor').css("background-color"))
+            
+        }
+    });
+
+    // ColorTag顯示（判斷滑鼠位置是否在div.colorlist內）
+    $(window).mousemove(function (e) {SetColorTagDisplay(e)});
 
     // 釘住ColorTag位置
     $('div.color_node').click(function () {
         pinned = !pinned;
-        $('div#copy').fadeToggle(100)
         if (pinned) {
-            $('div.largecolor').animate({
-                width: 150,
-                height: 150,
-            },100)
+            $('div#copy').css('display','inline-block')
         } else {
-            $('div.largecolor').animate({
-                width: 110,
-                height: 110,
-            },100)
+            $('div#copy').css('display','none')
+            SetColorTagPosition($(this))
+            SetLargeColor(
+                RGB2HEX($('div.color_node:hover').css("background-color")) 
+                + DEC2HEX(parseInt($('input.input-range').val()))
+            );
         }
     })
 
@@ -51,15 +64,12 @@ $(document).ready(function () {
         }  
     })
 
-    // ColorTag跟隨滑鼠
-    $(window).mousemove(function (e) {
-        MouseMoveInColorList(e)
-    });
-
     // 更改顏色透明度
     $('input.input-range').change(function() {
         let hexcolor =  RGB2HEX($('div.largecolor').css('background-color'))
-        $('div.largecolor').css('background-color',hexcolor + DEC2HEX($(this).val()))
+        // $('div.largecolor').css('background-color',hexcolor + DEC2HEX($(this).val()))
+        SetLargeColor(hexcolor + DEC2HEX($(this).val()))
+        $('span.rgb').html($('div.largecolor').css('background-color'))
     }) 
 
     // 視窗大小改變時畫面致中
@@ -70,16 +80,18 @@ $(document).ready(function () {
 // --------------   自訂Function    -----------------
 // --------------   自訂Function    -----------------
 // 數字轉顏色格式
-function DecimalToColorFormat(number) {
-    let colorstr = DEC2HEX(number)
+function NodeColorGen(number) {
+    if (number < 0)
+    {number= 0xFFFFFFFF + num + 1;}
+    let colorstr = number.toString(16).toUpperCase();
     colorstr = ("000" + colorstr).substr(-3)
     let colorarr = colorstr.split("")
-    let colorFormat = `${colorarr[2]}0${colorarr[1]}0${colorarr[0]}0`
+    let colorFormat = `${colorarr[2]}${colorarr[1]}${colorarr[0]}`
     return colorFormat
 }
 
 // 顯示ColorTag
-function MouseMoveInColorList(e) {
+function SetColorTagDisplay(e) {
     if (pinned) {
         $('div.colortag').css('display', 'inline-block')
         return;
@@ -95,6 +107,8 @@ function MouseMoveInColorList(e) {
         $('div.colortag').css('display', 'none')
     } else if (e.pageY < miny | e.pageY > maxy) {
         $('div.colortag').css('display', 'none')
+    } else if ($('div.color_node:hover').length == 0 & !pinned) {
+        $('div.colortag').css('display','none')
     } else {
         $('div.colortag').css('display', 'inline-block')
     }
@@ -102,25 +116,25 @@ function MouseMoveInColorList(e) {
 
 // 顏色展示區塊移動
 // TODO to make ColorTag moving smoothly...
-function ColorCodeInit() {
+function SetColorTagPosition(node) {
     let top_v;
     let left_v;
     if (pinned) {return}
-    $('div.colortag span')[0].innerHTML = $(this).css("background-color");
-    $('div.largecolor').css('background-color', $(this).css("background-color"));
-    if ( $(this).position().top > $(window).height()/2) {
-        top_v = $(this).position().top - 150
+    $('div.colortag span')[0].innerHTML = node.css("background-color");
+    // $('div.largecolor').css('background-color', $(this).css("background-color"));
+    if ( node.position().top > $(window).height()/2) {
+        top_v = node.position().top - 275
     } else {
-        top_v = $(this).position().top + 10
+        top_v = node.position().top + 20
     }
-    if ($(this).position().left > $(window).width()/2) {
-        left_v = $(this).position().left - 135
+    if (node.position().left > $(window).width()/2) {
+        left_v = node.position().left - 245
     } else {
-        left_v = $(this).position().left + 10
+        left_v = node.position().left + 25
     }
+
     $('div.colortag').css('top', top_v);
     $('div.colortag').css('left',left_v);
-    $('button#copy')[0].value = $(this).attr('alt')
 }
 
 // 畫面致中
@@ -134,9 +148,9 @@ function ToCenter() {
 // 顏色RGB格式 轉換 16進制格式
 function RGB2HEX(str) {
     // TODO　FIX RGBA regex pattern
-    // let regx = /^rgb\((\d{1,3}), (\d{1,3}), (\d{1,3})\)$/
-    let regx = /^rgb.*\((\d{1,3}), (\d{1,3}), (\d{1,3}).*\)/
-    let color_arr = str.match(regx)
+    let rgb_reg = /^rgba{0,1}\((\d{1,3}), {0,1}(\d{1,3}), {0,1}(\d{1,3})/
+    let color_arr = str.match(rgb_reg)
+
     let res = ''
     for (let i = 1;i < color_arr.length ; i ++) {
         res = res + DEC2HEX(parseInt(color_arr[i]))
@@ -145,8 +159,14 @@ function RGB2HEX(str) {
 }
 
 // 10進位變16進位
-function DEC2HEX(number){
-  if (number < 0)
-  {number = 0xFFFFFFFF + number + 1;}
-  return number.toString(16).toUpperCase();
+function DEC2HEX(number) {
+    let num = parseInt(number)
+    if (num < 0)
+    {num= 0xFFFFFFFF + num + 1;}
+    return ("0" + num.toString(16).toUpperCase()).substr(-2);
+    // return num.toString(16).toUpperCase();
+}
+
+function SetLargeColor(HEXCOLOR) {
+    $('div.largecolor').css('background-color',HEXCOLOR)
 }
